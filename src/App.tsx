e03b96b24386b5a77128783a2aaf1c5ca8e741f8
Gael Leonel Abreu <gaellabreu@Gaels-MacBook-Pro.local>
2019-11-14 19:25:42 -0400

    first commit - added antd with primary and secondary colors config, also added a basic layout to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Icon, Table } from 'antd';
+
+const { Header, Content } = Layout;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
+  return <Layout className="layout" style={{ height: 'inherit' }}>
+    <Header style={{ background: '#7200ca' }}>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px', background: 'transparent' }}>
+        <Menu.Item key="1"><Icon type={'caret-right'} />Eventos abiertos</Menu.Item>
+        <Menu.Item key="2"><Icon type={'container'} />Histórico</Menu.Item>
+        <Menu.Item key="3"><Icon type={'setting'} />Configuración</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '20px' }}>
+      <div style={{ background: '#fff', padding: 24, minHeight: '100%' }}>
+      <Table dataSource={dataSource} columns={columns} />;
+      </div>
+    </Content>
+  </Layout>
+    ;
 }
 
 export default App;
